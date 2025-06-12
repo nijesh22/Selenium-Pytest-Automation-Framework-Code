191c817e6c94b49672a7db5774f9c20a0b38ea9d
nijesh22 <imnijeshmanuel@gmail.com>
2025-06-12 10:32:54 +0530

    duplicate products testcase

---
+++ tests/Product_Listing_Page_TC_20/test_Validate_no_duplicate_products.py
@@ -0,0 +1,24 @@
+import pytest
+from selenium.webdriver.support.ui import WebDriverWait
+from Pages.LoginPage import LoginPage
+from Pages.HomePage import HomePage
+
+@pytest.mark.skip(reason="Skipping temporarily – avoids confusion")
+@pytest.mark.usefixtures("setup")
+class TestValidateNoDuplicateProducts:
+    def testValidateNoDuplicateProducts_1(self):
+
+        wait = WebDriverWait(self.driver, 10)
+
+        login_page = LoginPage(self.driver, wait)
+        login_page.swag_labs_loginIsvalid("standard_user", "secret_sauce")
+        login_page.swag_labs_login_button()
+
+        home_page = HomePage(self.driver, wait)
+        product_names = home_page.get_product_names()
+
+        product_names_set = set(product_names)
+
+        assert len(product_names) == len(product_names_set), "❌ Duplicate product names found!"
+
+        print("✅ No duplicate products found.")
